7a51d25ce1ce724e3d626e1d0016fc29c8433e74
Deadpool3413 <j.rivas4818@gmail.com>
2022-07-25 18:29:18 -0700

    pushing to heroku

--- tac-toe/apps/dashboard/src/app/app.module.ts
+++ tac-toe/apps/dashboard/src/app/app.module.ts
@@ -10,6 +10,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NbThemeModule, NbLayoutModule, NbButtonModule } from '@nebular/theme';
 import { NbEvaIconsModule } from '@nebular/eva-icons';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -24,7 +26,12 @@ import { NbEvaIconsModule } from '@nebular/eva-icons';
     NbThemeModule.forRoot({ name: 'cosmic' }), 
     NbLayoutModule,
     NbButtonModule,  
-    NbEvaIconsModule],
+    NbEvaIconsModule, ServiceWorkerModule.register('ngsw-worker.js', {
+  enabled: environment.production,
+  // Register the ServiceWorker as soon as the application is stable
+  // or after 30 seconds (whichever comes first).
+  registrationStrategy: 'registerWhenStable:30000'
+})],
   providers: [],
   bootstrap: [AppComponent],
 })
